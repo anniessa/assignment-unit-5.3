8cba5b6466981dad2cd543150d61363b65227b30
Anniessa Antar <anniessa.antar@gmail.com>
2023-01-10 16:49:58 -0600

    updated function with for loop. Itnow lists album's information, but it also is saying undefined

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -25,9 +25,23 @@ console.log('You just added ', addToCollection('Channel Tres', 'Channel Tres', 2
 
 console.log(collection);
 
+//Add a function named showCollection. This function should:
+// Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
+// Console.log the number of items in the array.
+// Loop over the array and console.log each album's information formatted like: TITLE by ARTIST, published in YEAR.
+
 function showCollection (array) {
-    for (i in array){
-        console.log(title, 'by', artist, 'published in ', yearPublished);
+    for (let i =0; i<array.length; i++){
+        array[i];
+        console.log(array[i]);
     }
 }
-console.log(showCollection[collection[albumElements]]);
+//Test the showCollection function. 
+console.log(showCollection(collection));
+
+//Add a function named findByArtist. This function should:
+// Take in artist (a string) parameter
+// Create an array to hold any results, empty to start
+// Loop through the collection and add any objects with a matching artist to the array.
+// Return the array with the matching results. If no results are found, return an empty array.
+// Test the findByArtist function. Make sure to test with an artist you know is in the collection, as well as an artist you know is not in your collection. Check that for artists with multiple matches, all are found.
