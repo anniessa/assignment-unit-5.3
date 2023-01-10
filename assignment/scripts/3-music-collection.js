9e481f3aead62040b45ea6f19353689a331b8873
Anniessa Antar <anniessa.antar@gmail.com>
2023-01-10 11:52:52 -0600

    fixed the log to make sure the addToCollection was calling something

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,14 +1,26 @@
 console.log('***** Music Collection *****')
 let collection = [];
+let albumElements = {
+    title: '',
+    artist: '',
+    yearPublished: 0,
+}
 
 function addToCollection (title, artist, yearPublished) {
     let albumElements = {
-        albumTitle: '',
-        albumArtist: '',
-        albumYearPublished: 0,
-    } 
-    albumElements.push(collection);
-    return albumElements;
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+    }    
+        collection.push(albumElements);
+        return albumElements;
     }
-addToCollection('Oil of Every Pearls Undersides', 'SOPHIE', 2018);
-console.log('You just added ', addToCollection);
+
+console.log('You just added ', addToCollection("Oil of Every Pearl's Undersides", 'SOPHIE', 2018));
+console.log('You just added ', addToCollection("EP2", 'Yaeji', 2017));
+console.log('You just added ', addToCollection('Live!', 'Bob Marley', 1975));
+console.log('You just added ', addToCollection('Dreamland', 'Black Box', 1990));
+console.log('You just added ', addToCollection('Little Dragon', 'Little Dragon', 2007));
+console.log('You just added ', addToCollection('Channel Tres', 'Channel Tres', 2018));
+
+console.log(collection);
