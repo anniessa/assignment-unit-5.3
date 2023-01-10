3a2916153e052a15978b46cbd4e5fcb61caf7377
Anniessa Antar <anniessa.antar@gmail.com>
2023-01-10 10:52:53 -0600

    added function addToCollection which will push albumElements object into collection array and return albumElements

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,12 @@
 console.log('***** Music Collection *****')
 let collection = [];
 
+function addToCollection (title, artist, yearPublished) {
+    let albumElements = {
+        albumTitle: '',
+        albumArtist: '',
+        albumYearPublished: 0,
+    } 
+    collection.push(albumElements);
+    return albumElements
+    }
